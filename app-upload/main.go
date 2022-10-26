28d1e9ea637fe959b364b99c0225ac904a12fb57
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-26 22:59:00 +0530

    listen and serve on port 8080

--- app-upload/main.go
+++ app-upload/main.go
@@ -1,14 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	mux := routes()
-}
 
+	log.Println("Starting server on port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+// uses standard library to handle 3 routes
 func routes() http.Handler{
 	mux := http.NewServeMux()
-	// register handler for default page
+	// register handler for default route
 	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
 	// register handler for uplaodFiles
 	mux.HandleFunc("/upload", uploadFiles)
