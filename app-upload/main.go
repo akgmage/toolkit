5f1c984ab393c4033f66d3ef07be123b128acc66
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-27 22:25:04 +0530

    return method not allowed if method is not post

--- app-upload/main.go
+++ app-upload/main.go
@@ -30,7 +30,9 @@ func routes() http.Handler{
 }
 // upload multiple files
 func uploadFiles(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 // upload one file
 func uploadOneFile(w http.ResponseWriter, r *http.Request) {
