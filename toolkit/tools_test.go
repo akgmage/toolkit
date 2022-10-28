03a990ec78dc75fc91d6b3a9f0687ea5e360dce8
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-28 22:17:53 +0530

    add TestTools_UploadFiles func and set up pipe to avoid buffering

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -1,8 +1,13 @@
 package toolkit
 
-import "testing"
+import (
+	"io"
+	"mime/multipart"
+	"testing"
+)
+
 // Function start with name Test followed by receiver Tools
-// and _ and name of function i.e. RandomString 
+// and _ and name of function i.e. RandomString
 // takes a param t of type *testing.T (pointer to testing.T)
 func TestTools_RandomString(t *testing.T) {
 	var testTools Tools
@@ -20,4 +25,12 @@ var uploadTests = []struct{
 	errorExpected bool
 }{
 	{name: "allowed no rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: false, errorExpected: false},
+}
+
+func TestTools_UploadFiles(t *testing.T) {
+	for _, e := range uploadTests{
+		// set up pipe to avoid buffering
+		pr, pw := io.Pipe()
+		writer := multipart.NewWriter(pw)
+	}
 }
