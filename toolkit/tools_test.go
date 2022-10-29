5547f2a9fcb6e0ce7503c4c14a328ae35a828363
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-29 22:24:16 +0530

    handle error

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -89,5 +89,9 @@ func TestTools_UploadFiles(t *testing.T) {
 			}
 			_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName))
 		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s: error expected but none received", e.name)
+		}
 	}
 }
