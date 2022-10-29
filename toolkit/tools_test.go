85b5fa4d2f65f9e956e79e03fb15631434db2a64
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-29 22:29:47 +0530

    add comment

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -86,6 +86,7 @@ func TestTools_UploadFiles(t *testing.T) {
 		if !e.errorExpected {
 			// Stat returns a FileInfo describing the named file. If there is an error, it will be of type *PathError
 			if _, err := os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName)); os.IsNotExist(err) {
+				// Errorf is equivalent to Logf followed by Fail.
 				t.Errorf("%s: expected file to exist: %s", e.name, err.Error())
 			}
 			// Remove removes the named file or directory. If there is an error, it will be of type *PathError.
