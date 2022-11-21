a5c071466b4f7babe1a08729d28ad7ac9d6716f9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-21 22:46:50 +0530

    add japanese + roman text to table test

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -186,6 +186,7 @@ var slugTests = []struct {
 	{name: "", s: "now is the time", expected: "", errorExpected: true},
 	{name: "complex string", s: "Now is the time! + Goal &123 ^", expected: "now-is-the-time-goal-123", errorExpected: true},
 	{name: "Japanese string", s: "こんにちは世界", expected: "", errorExpected: true},
+	{name: "Japanese string", s: "こんにちは世界hello world", expected: "hello-world", errorExpected: false},
 }
 
 func TestTools_Slugify(t *testing.T) {
